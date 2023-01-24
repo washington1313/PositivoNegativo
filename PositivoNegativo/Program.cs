5d2639b4db499a9c498616eaa70e99d2040654ea
Washinfron Fernandes <wmfmg@hotmail.com>
2023-01-24 18:09:31 -0300

    Projeto Finalizado

---
+++ PositivoNegativo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace PositivoNegativo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+
+            Console.WriteLine("Digite um número: ");
+
+            numero = int .Parse(Console.ReadLine());
+
+            if (numero >=0 )
+            {
+                Console.WriteLine("*** Número Positivo ***");
+            }
+
+            else if (numero < 0)
+            {
+                Console.WriteLine("*** Número Negativo ***");
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
